fff2c95b8c0eddd6d0accffa935794a7904cfdf6
Kedicik1234--email <burakcanciftci1@gmail.com>
2019-12-19 11:31:00 +0300

    Block Method Update

--- The CounterKnight/Assets/Resources/Scripts/Character.cs
+++ The CounterKnight/Assets/Resources/Scripts/Character.cs
@@ -3,25 +3,10 @@ using UnityEngine;
 
 public class Character : Person
 {
-   public GameObject x;
-   public GameObject y;
-
    public override void Die()
    {
        Destroy(gameObject);
        Score.saveBestScore();
        SceneManager.LoadScene(0, LoadSceneMode.Single);
-   }
-
-   public void openLeftBlock()
-   {
-       x.SetActive(true);
-       y.SetActive(false);
-   }
-
-   public void openRightBlock()
-   {
-       y.SetActive(true);
-       x.SetActive(false);
    }
 }
